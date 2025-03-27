df54708c038580aeb04e8d85482a8c00eff5e0a5
Royjith <153445207+Royjith@users.noreply.github.com>
2025-03-27 16:28:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,20 +78,20 @@ output "datastore_id" {
 }
 
 resource "vsphere_tag_category" "category" {
-  name             = "Kubernetes6"
+  name             = "Kubernetes7"
   description      = "Category for Kubernetes VMs"
   cardinality      = "MULTIPLE"
   associable_types = ["VirtualMachine"]
 }
 
 resource "vsphere_tag" "master" {
-  name        = "master6"
+  name        = "master7"
   description = "Tag for master VMs"
   category_id = vsphere_tag_category.category.id
 }
 
 resource "vsphere_tag" "worker" {
-  name        = "worker6"
+  name        = "worker7"
   description = "Tag for worker VMs"
   category_id = vsphere_tag_category.category.id
 }
